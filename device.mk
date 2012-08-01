cd40aca632d5f3c64226b16abd870db25d70312a
Akihiro Maeda <sola.1980.a@gmail.com>
2012-08-01 23:08:47 +0900

    fix facelock. small bug fixes.

--- device.mk
+++ device.mk
@@ -192,6 +192,33 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=320
 
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.ringtone=Themos.ogg \
+	ro.config.notification_sound=Proxima.ogg \
+	ro.config.alarm_alert=Cesium.ogg \
+	ro.com.android.dateformat=MM-dd-yyyy \
+	ro.com.android.dataroaming=false \
+	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+	ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+	ro.com.google.clientidbase=android-google \
+	ro.carrier=unknown \
+	ro.com.android.wifi-watchlist=GoogleGuest \
+	ro.error.receiver.system.apps=com.google.android.feedback \
+	ro.setupwizard.enterprise_mode=1 \
+	keyguard.no_require_sim=true \
+	drm.service.enabled=true \
+	ro.facelock.black_timeout=1250 \
+	ro.facelock.det_timeout=1500 \
+	ro.facelock.rec_timeout=2500 \
+	ro.facelock.lively_timeout=2500 \
+	ro.facelock.est_max_time=1000 \
+	ro.facelock.use_intro_anim=true \
+	camera.flash_off=0 \
+	ro.com.google.mcc_fallback=262
+
+PRODUCT_PACKAGES += \
+	Stk
+
 PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_TAGS += dalvik.gc.type-precise
